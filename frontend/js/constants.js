55f7f76942c2fa5e953a2d523861fbae14c6b092
GraveyardMotel <cooperjack6567@gmail.com>
2022-04-14 08:33:37 +1000

    updated colours

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x2CD9f34fb3bB7C15CD7e8DFB0f43f8738D33a82f"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Graveyard Motel NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Graveyard Motel NFT Project is great for people who are just getting into the NFT space';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
